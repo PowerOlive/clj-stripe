744562b477c867b740b54496f627556bd229a0a3
Ulysses Aalto <uaalto@getlantern.org>
2015-11-30 15:35:35 -0500

    Change version so clojars is happy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject getlantern/clj-stripe "1.0.5"
+(defproject getlantern/clj-stripe "1.0.6"
   :description "Clojure binding for Stripe"
   :dependencies [[org.clojure/clojure "1.5.0"]
 		 [org.clojure/data.json "0.1.3"]
