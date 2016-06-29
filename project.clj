7e510bf39536f49cb6c8d96a0d3fa5a20076d402
Ulysses Aalto <uaalto@getlantern.org>
2016-06-29 22:25:59 +0200

    Back to old Clojure for the time being

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject getlantern/clj-stripe "1.0.5-SNAPSHOT"
   :description "Clojure binding for Stripe"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-		 [clj-http "2.0.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]
+		 [clj-http "0.9.1"]])
